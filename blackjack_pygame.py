f1a24cc499e25d9a57eb51ec7121399c76c7452a
Arnold Joseph <149133106+Ulysses013@users.noreply.github.com>
2023-12-06 13:57:37 +0100

    Create blackjack_pygame.py

---
+++ blackjack_pygame.py
@@ -0,0 +1,13 @@
+import pygame as pygame
+from blackjack_deck import *
+from constants import *
+import time
+pygame.init()
+
+clock = pygame.time.Clock()
+
+gameDisplay = pygame.display.set_mode((display_width, display_height))
+
+pygame.display.set_caption('BlackJack')
+gameDisplay.fill(background_color)
+pygame.draw.rect(gameDisplay, grey, pygame.Rect(0, 0, 250, 700))
